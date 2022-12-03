0e39f59e9a8c6a355aab0f1f77a7e95489881a8a
SLW badsty <111683903+Badsty@users.noreply.github.com>
2022-12-03 19:58:04 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# SLW | Dev - Leak
+
+Developed by Badsty
+Discord : https://discord.gg/W6mTKpG5Jn
+
+Touche : Alt + Clique Gauche 
+Entièrement configurable 
+
+# 2022 - 2023
+
---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+client_scripts {
+	'screenToWorld.lua',
+
+	'Drawables/Color.lua',
+	'Drawables/Rect.lua',
+	'Drawables/Text.lua',
+	'Drawables/Sprite.lua',
+
+	'Menu/Item.lua',
+	'Menu/TextItem.lua',
+	'Menu/CheckboxItem.lua',
+	'Menu/SubmenuItem.lua',
+	'Menu/Separator.lua',
+	'Menu/Border.lua',
+	'Menu/Menu.lua',
+	'Menu/MenuPool.lua',
+
+	'example.lua'
+}
